d2df386bba87d6c00bc8203a3749ceb023da6d14
Aditi <aditideshpande2908@gmail.com>
2024-08-07 15:01:50 +0530

    style result section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{html,js}'],
 	theme: {
-		extend: {},
+		extend: {
+			width: {
+				128: '32rem',
+			},
+			colors: {
+				'slate-blue': 'hsl(252, 100%, 67%)',
+				'royal-blue': 'hsl(241, 81%, 54%)',
+				'violet-blue': 'hsla(256, 72%, 46%, 1)',
+				'persian-blue': 'hsla(241, 72%, 46%, 0)',
+			},
+		},
+		fontFamily: {
+			sans: ['Hanken Grotesk'],
+		},
 	},
 	plugins: [],
 }
